fe0eea58b57e955200dbf5bdcdad40df1c36fae1
Adebola Adesina <dr3cod3form3@outlook.com>
2020-07-21 06:51:05 -0700

    Linear searhch  done

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,18 @@
+#!/usr/bin/env python3jiopP
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return 1
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
+
+
 def binary_search(arr, target):
 
     # Your code here
-
 
     return -1  # not found
