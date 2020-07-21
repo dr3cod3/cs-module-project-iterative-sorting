8611e89a18a3aeb6c7910743aaae37fbd1c2536f
Adebola Adesina <dr3cod3form3@outlook.com>
2020-07-21 07:38:05 -0700

    MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -3,7 +3,7 @@ def linear_search(arr, target):
     # Your code here
     for i in range(len(arr)):
         if arr[i] == target:
-            return 1
+            return i
 
     return -1   # not found
 
@@ -12,6 +12,23 @@ def linear_search(arr, target):
 
 
 def binary_search(arr, target):
+    ## divide in half
+    # if equals, return true
+    # if smaller, return left half
+    # else return right target
+    # if at the end, return false
+
+    left_index = 0
+    right_index = len(arr)-1
+    # length = len(arr)
+    while right_index - left_index > 0:
+        mid_index = (left_index + right_index) // 2
+        if arr[mid_index] > target:
+            right_index = mid_index
+        elif arr[mid_index] < target:
+            left_index = mid_index
+        else:
+            return mid_index
 
     # Your code here
 
